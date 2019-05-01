30935d84687f9c904933de4a0db8ec37e5da5bc8
datapsyche <jithinjkumar@gmail.com>
2019-05-01 18:24:17 +0530

    added changes

--- helloworld.js
+++ helloworld.js
@@ -7,3 +7,33 @@ console.log(1+1)
 //printing value of a variable in console
 const a = 10
 console.log(`${a}`)
+
+//print error message
+console.error('This is an error');
+
+//print warning message
+console.warn('This is a warning');
+
+// setting variables
+// three ways of doing it
+
+// var
+var b = 10
+console.log(`${a}`);
+
+//const
+const c = 20
+console.log(`${c}`);
+
+//let
+let d =30
+console.log(`${d}`);
+
+//data types - String, Numbers, Boolean, null, undefined
+
+
+
+
+
+
+
