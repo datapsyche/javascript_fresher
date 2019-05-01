0bae1abcecd06937b727c06bb1e7227e7c265b2c
datapsyche <jithinjkumar@gmail.com>
2019-05-01 16:37:00 +0530

    frist draft

---
+++ helloworld.js
@@ -0,0 +1,12 @@
+// printing hello world in console
+console.log("Hello World!");
+
+//printing sum of two numbers in console
+console.log(1+1)
+
+//printing value of a variable in console
+const a = 10
+console.log(`${a}`)
+
+
+
