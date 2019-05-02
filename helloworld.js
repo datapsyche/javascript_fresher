c2c817b33a1567a3d3db71ed4e107de7eaa8f87d
datapsyche <jithinjkumar@gmail.com>
2019-05-02 09:34:24 +0530

    Day-1

--- helloworld.js
+++ helloworld.js
@@ -1,38 +1,73 @@
-// printing hello world in console
-console.log("Hello World!");
+// // printing hello world in console
+// console.log("Hello World!");
 
-//printing sum of two numbers in console
-console.log(1+1)
+// //printing sum of two numbers in console
+// console.log(1+1)
 
-//printing value of a variable in console
-const a = 10
-console.log(`${a}`)
+// //printing value of a variable in console
+// const a = 10
+// console.log(`${a}`)
 
-//print error message
-console.error('This is an error');
+// //print error message
+// console.error('This is an error');
 
-//print warning message
-console.warn('This is a warning');
+// //print warning message
+// console.warn('This is a warning');
 
-// setting variables
-// three ways of doing it
+// // setting variables
+// // three ways of doing it
 
-// var
-var b = 10
-console.log(`${a}`);
+// // var
+// var b = 10
+// console.log(`${a}`);
 
-//const
-const c = 20
-console.log(`${c}`);
+// //const
+// const c = 20
+// console.log(`${c}`);
 
-//let
-let d =30
-console.log(`${d}`);
+// //let
+// let d = 30;
+// d = 31;
+// console.log(`${d}`);
 
-//data types - String, Numbers, Boolean, null, undefined
+// //data types - String, Numbers, Boolean, null, undefined and Symbol
 
+// const name = 'john';
+// const age = 28;
+// const rating = 4.5;
+// const isemployed = true;
+// const x = null;
+// const y = undefined;
+// let z;
 
+// console.log('name is of type:', typeof name);
+// console.log('age is of type:', typeof age);
+// console.log('rating is of type:', typeof rating);
+// console.log('isemployed is of type:', typeof isemployed);
+// console.log('x is of type:', typeof x);
+// console.log('y is of type:', typeof y);
+// console.log('z is of type:', typeof z);
 
+// // concatenation
+// console.log('My name is ',name,' and iam ',age);
+// console.log('My name is ' + name+' and iam '+ age);
+// console.log(`My name is ${name} and I'm ${age}`);
+
+// const sentence = `My name is ${name} and I'm ${age}`;
+// console.log(sentence);
+
+// const s = 'Hello World';
+// console.log(s.length);
+// console.log(s.toUpperCase());
+// console.log(s.substring(0,5));
+// console.log(s.substring(0, 5).toLowerCase());
+// console.log(s.split(' '));
+
+// const tags = 'Technology, Computers, IT, Code'
+// console.log(tags.toLowerCase().split(' '));
+
+// Arrays
+const numbers  = new 
 
 
 
